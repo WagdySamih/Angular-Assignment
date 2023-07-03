e2ec5ac20b2e60621e6154a1b00dfb66cec2d894
Wagdy Samih <wagdysamih787@gmail.com>
2023-07-03 23:35:29 +0300

    Feat: implements user card component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, UserCardComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
@@ -17,16 +23,24 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatTooltipModule,
   ],
   exports: [
     HeaderComponent,
     FooterComponent,
+    UserCardComponent,
+
     MatToolbarModule,
     MatToolbarModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatTooltipModule,
   ],
 })
 export class SharedModule {}
