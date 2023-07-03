c325f65f5b89ea05a3aaf1d3ec712a75e231dbea
Wagdy Samih <wagdysamih787@gmail.com>
2023-07-03 16:52:04 +0300

    Feat: Adds Core to app module,  call create bulk users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '@core/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Angular-Vicoland-Assignment';
+export class AppComponent implements OnInit {
+  title = 'Vicoland Assignment';
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.addMultipleUsers().subscribe({
+      next: (users) => console.log('Users added:', users),
+      error: (error) => console.error('Error adding users:', error),
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from '@shared/shared.module';
+import { CoreModule } from '@core/core.module';
 
 import { AppComponent } from './app.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
