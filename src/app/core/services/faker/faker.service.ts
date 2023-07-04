02bbf53b07b1943918312237c4f4a1fb9595cd7f
Wagdy Samih <wagdysamih787@gmail.com>
2023-07-04 03:24:43 +0300

    Refactor: Implements faker service to handle create fake users

---
+++ src/app/core/services/faker/faker.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { faker } from '@faker-js/faker';
+
+import { User } from '@app/core/models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FakerService {
+  generateFakeUser(): User {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.firstName();
+    return {
+      _id: faker.string.uuid(),
+      firstName,
+      lastName,
+      avatar: faker.image.avatar(),
+      email: firstName + '.' + lastName + '@gmail.com',
+      street: faker.location.street(),
+      city: faker.location.city(),
+      country: faker.location.country(),
+      zipCode: faker.location.zipCode(),
+    };
+  }
+}
