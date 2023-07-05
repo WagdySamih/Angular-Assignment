bc185433da3fb8e2361114016520fc8f998e261f
Wagdy Samih <wagdysamih787@gmail.com>
2023-07-05 23:33:21 +0300

    Feat: Adds error handling to faker service

--- src/app/core/services/faker/faker.service.ts
+++ src/app/core/services/faker/faker.service.ts
@@ -3,34 +3,49 @@ import { faker } from '@faker-js/faker';
 
 import { User } from '@app/core/models';
 import { ImagesService } from '../images/images.service';
+import { LoggerService } from '../logger/logger.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FakerService {
-  constructor(private imagesService: ImagesService) {}
+  constructor(
+    private imagesService: ImagesService,
+    private logger: LoggerService
+  ) {}
 
   generateFakeUser(): User {
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.lastName();
-    return {
-      _id: faker.string.uuid(),
-      firstName,
-      lastName,
-      avatar: this.imagesService.selectRandomImage(),
-      email: (firstName + '.' + lastName + '@gmail.com').toLowerCase(),
-      street: faker.location.street(),
-      city: faker.location.city(),
-      country: faker.location.country(),
-      zipCode: faker.location.zipCode().replace(/-/g, ''),
-    };
+    try {
+      const firstName = faker.person.firstName();
+      const lastName = faker.person.lastName();
+      return {
+        _id: faker.string.uuid(),
+        firstName,
+        lastName,
+        avatar: this.imagesService.selectRandomImage(),
+        email: (firstName + '.' + lastName + '@gmail.com').toLowerCase(),
+        street: faker.location.street(),
+        city: faker.location.city(),
+        country: faker.location.country(),
+        zipCode: faker.location.zipCode().replace(/-/g, ''),
+      };
+    } catch (error) {
+      this.logger.error(error);
+      throw error;
+    }
   }
 
   generateFakeUsers(count: number): User[] {
-    const users: User[] = [];
-    for (let i = 0; i < count; i++) {
-      users.push(this.generateFakeUser());
+    try {
+      const users: User[] = [];
+      for (let i = 0; i < count; i++) {
+        const user = this.generateFakeUser();
+        user && users.push(user);
+      }
+      return users;
+    } catch (error) {
+      this.logger.error(error);
+      throw error;
     }
-    return users;
   }
 }
